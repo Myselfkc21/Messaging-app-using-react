1f5df90418afd6928aa4ac1ce4ee15b6e2676cc9
krishna chaitanya <arekapudikrishnachaitanya@gmail.com>
2024-09-05 00:15:01 +0530

    ui done

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -3,7 +3,103 @@ import './Details.css'
 const Details = () => {
   return (
     <div className="Details">
-      Details
+      <div className="user">
+        <img src="./avatar.png" alt="" />
+        <h2>Jone Donr</h2>
+        <p>Lorem ipsum dolor sit, amet dl consectet ldvvkdldv sdmvmdvmdl</p>
+      </div>
+      <div className="info">
+      <div className="options">
+          <div className="title">
+            <span>Chat Settings</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="options">
+          <div className="title">
+            <span>Privacy and help</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        
+        <div className="options">
+          <div className="title">
+            <span>Shared Photos</span>
+            <img src="./arrowDown.png" alt="" />
+          </div>
+          <div className="photos">
+            <div className="photoItem">
+              <div className="photoDetail">
+              <img src="https://variety.com/wp-content/uploads/2023/11/The-Weeknd-2.jpg" alt="" />
+              <span>Weeknd_01.png</span>
+              </div>
+            <img className='download' src="./download.png" alt="" />
+            </div>
+          </div>  
+          <div className="photos">
+            <div className="photoItem">
+              <div className="photoDetail">
+              <img src="https://variety.com/wp-content/uploads/2023/11/The-Weeknd-2.jpg" alt="" />
+              <span>Weeknd_01.png</span>
+              </div>
+            <img className='download' src="./download.png" alt="" />
+            </div>
+          </div>  
+          <div className="photos">
+            <div className="photoItem">
+              <div className="photoDetail">
+              <img src="https://variety.com/wp-content/uploads/2023/11/The-Weeknd-2.jpg" alt="" />
+              <span>Weeknd_01.png</span>
+              </div>
+            <img className='download' src="./download.png" alt="" />
+            </div>
+          </div>  
+          <div className="photos">
+            <div className="photoItem">
+              <div className="photoDetail">
+              <img src="https://variety.com/wp-content/uploads/2023/11/The-Weeknd-2.jpg" alt="" />
+              <span>Weeknd_01.png</span>
+              </div>
+            <img className='download' src="./download.png" alt="" />
+            </div>
+          </div>  
+          <div className="photos">
+            <div className="photoItem">
+              <div className="photoDetail">
+              <img src="https://variety.com/wp-content/uploads/2023/11/The-Weeknd-2.jpg" alt="" />
+              <span>Weeknd_01.png</span>
+              </div>
+            <img className='download' src="./download.png" alt="" />
+            </div>
+          </div>  
+          <div className="photos">
+            <div className="photoItem">
+              <div className="photoDetail">
+              <img src="https://variety.com/wp-content/uploads/2023/11/The-Weeknd-2.jpg" alt="" />
+              <span>Weeknd_01.png</span>
+              </div>
+            <img className='download' src="./download.png" alt="" />
+            </div>
+          </div>  
+          <div className="photos">
+            <div className="photoItem">
+              <div className="photoDetail">
+              <img src="https://variety.com/wp-content/uploads/2023/11/The-Weeknd-2.jpg" alt="" />
+              <span>Weeknd_01.png</span>
+              </div>
+            <img className='download' src="./download.png" alt="" />
+            </div>
+          </div>  
+        </div>
+
+        <div className="options">
+          <div className="title">
+            <span>Shared files</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <button>Block user</button>
+      </div>
     </div>
   )
 }
